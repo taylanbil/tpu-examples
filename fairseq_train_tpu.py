3b5c444dc1c4b82c995776ad611c40003e70eab1
Taylan Bilal <taylanbil@google.com>
2019-08-02 17:26:48 +0000

    time print + hf

--- fairseq_train_tpu.py
+++ fairseq_train_tpu.py
@@ -179,7 +179,7 @@ def prepare_task(args, devices):
 
   # Build models and criteria to print some metadata
   model_parallel = dp.DataParallel(
-      lambda: task.build_model(args), device_ids=devices)
+      lambda: task.build_model(args), device_ids=devices, hf=False)
   model, criterion = task.build_model(args), task.build_criterion(args)
   print(model)
   print('| model {}, criterion {}'.format(args.arch,
@@ -209,6 +209,7 @@ def prepare_task(args, devices):
 
 
 def now():
+  return datetime.now()
   return datetime.now().strftime('%H:%M:%S')
 
 def count_compiles():
